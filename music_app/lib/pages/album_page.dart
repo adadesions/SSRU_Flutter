498ee846a76c56d7470fc117d2c3f735c359209c
adadesions <adadesions@gmail.com>
2023-05-03 22:49:47 +0700

    Separate album list data to album_list.dart

--- music_app/lib/pages/album_page.dart
+++ music_app/lib/pages/album_page.dart
@@ -1,44 +1,10 @@
 import 'package:flutter/material.dart';
 
 // Models
-import '../models/album.dart';
+// import '../models/album.dart';
 
-final List<Album> albumList = [
-  Album(
-      title: "Jaff Satur",
-      artist: "Jaff Satur",
-      description: "Artist under Wayfer Records, Warner Music Thailand.",
-      numSongs: 12,
-      imgUrl: "assets/images/albums/jeff_cover.jpg"),
-  Album(
-      title: "Gaze",
-      artist: "Ice Saranyu",
-      description:
-          "An album designed for late-night listening, with a mix of mellow and upbeat tracks.",
-      numSongs: 9,
-      imgUrl: "assets/images/albums/ice_saranyu_cover.jpg"),
-  Album(
-      title: "Metamorphosis",
-      artist: "Palmy",
-      description:
-          "An album that explores the different stages of love and relationships, with a mix of electronic and acoustic sounds.",
-      numSongs: 11,
-      imgUrl: "assets/images/albums/palmy_cover.jpg"),
-  Album(
-      title: "Monologue",
-      artist: "Stamp Apiwat",
-      description:
-          "A solo album by Stamp Apiwat, with introspective lyrics and a mix of pop and rock sounds.",
-      numSongs: 10,
-      imgUrl: "assets/images/albums/stamp_apiwat_cover.jpg"),
-  Album(
-      title: "The Revolution",
-      artist: "Slot Machine",
-      description:
-          "An album that blends classic rock and modern pop sounds, with politically charged lyrics.",
-      numSongs: 9,
-      imgUrl: "assets/images/albums/slot_machine_cover.jpg"),
-];
+// Data
+import '../data/album_list.dart';
 
 class AlbumPage extends StatefulWidget {
   const AlbumPage({super.key});
