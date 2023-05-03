25b9b9f102cb5f68548298f2d402c85f727522d4
adadesions <adadesions@gmail.com>
2023-05-03 22:44:18 +0700

    Adjust ListTile

--- music_app/lib/pages/album_page.dart
+++ music_app/lib/pages/album_page.dart
@@ -50,21 +50,46 @@ class AlbumPage extends StatefulWidget {
 class _AlbumPageState extends State<AlbumPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Albums"),
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/bg_musicapp.jpg'),
+            fit: BoxFit.cover),
       ),
-      body: ListView.builder(
-        itemCount: albumList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Image.asset(albumList[index].imgUrl),
-            title: Text(albumList[index].title),
-            subtitle: Text(albumList[index].description),
-            trailing: Text('${albumList[index].numSongs.toString()} Songs'),
-            isThreeLine: true,
-          );
-        },
+      child: Scaffold(
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          title: const Text("Albums"),
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(50, 255, 89, 0),
+          shape: const RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(120))),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.only(top: 20),
+          itemCount: albumList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Ink(
+              color: const Color.fromARGB(10, 255, 89, 0),
+              child: ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                horizontalTitleGap: 20,
+                minVerticalPadding: 5,
+                leading: Image.asset(albumList[index].imgUrl),
+                title: Text(
+                    '${albumList[index].artist}, ${albumList[index].title}'),
+                subtitle: Text(albumList[index].description),
+                trailing: Text('${albumList[index].numSongs.toString()} Songs'),
+                isThreeLine: true,
+                dense: true,
+                // visualDensity: VisualDensity(horizontal: -4, vertical: 4),
+                onTap: () {},
+              ),
+            );
+          },
+        ),
       ),
     );
   }
