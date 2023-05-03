62fae509b51979d655eac2d7415a1d9242084d86
adadesions <adadesions@gmail.com>
2023-05-03 22:05:00 +0700

    Update albums cover and its data

--- music_app/lib/pages/album_page.dart
+++ music_app/lib/pages/album_page.dart
@@ -10,6 +10,34 @@ final List<Album> albumList = [
       description: "Artist under Wayfer Records, Warner Music Thailand.",
       numSongs: 12,
       imgUrl: "assets/images/albums/jeff_cover.jpg"),
+  Album(
+      title: "Gaze",
+      artist: "Ice Saranyu",
+      description:
+          "An album designed for late-night listening, with a mix of mellow and upbeat tracks.",
+      numSongs: 9,
+      imgUrl: "assets/images/albums/ice_saranyu_cover.jpg"),
+  Album(
+      title: "Metamorphosis",
+      artist: "Palmy",
+      description:
+          "An album that explores the different stages of love and relationships, with a mix of electronic and acoustic sounds.",
+      numSongs: 11,
+      imgUrl: "assets/images/albums/palmy_cover.jpg"),
+  Album(
+      title: "Monologue",
+      artist: "Stamp Apiwat",
+      description:
+          "A solo album by Stamp Apiwat, with introspective lyrics and a mix of pop and rock sounds.",
+      numSongs: 10,
+      imgUrl: "assets/images/albums/stamp_apiwat_cover.jpg"),
+  Album(
+      title: "The Revolution",
+      artist: "Slot Machine",
+      description:
+          "An album that blends classic rock and modern pop sounds, with politically charged lyrics.",
+      numSongs: 9,
+      imgUrl: "assets/images/albums/slot_machine_cover.jpg"),
 ];
 
 class AlbumPage extends StatefulWidget {
