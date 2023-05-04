74636bf9cde334023d220bf62f23f7d04ced69b8
adadesions <adadesions@gmail.com>
2023-05-04 13:24:53 +0700

    Recreate Task data struction and add new data to task_list

--- todo_app/lib/data/task_list.dart
+++ todo_app/lib/data/task_list.dart
@@ -1,7 +1,34 @@
 import '../models/task.dart';
 
 List<Task> taskList = [
-  Task(id: 0, body: 'Teaching states in flutter', status: Status.process),
-  Task(id: 1, body: 'Implementing user authentication', status: Status.process),
-  Task(id: 2, body: 'Creating a responsive UI design', status: Status.process),
+  Task(
+      id: 0,
+      body: 'Teaching states in flutter',
+      status: Status.process,
+      deadline: "04/05/2023",
+      flag: Flags.personal),
+  Task(
+      id: 1,
+      body: 'Implementing user authentication',
+      status: Status.done,
+      deadline: "05/31/2023",
+      flag: Flags.work),
+  Task(
+      id: 2,
+      body: 'Creating a responsive UI design',
+      status: Status.done,
+      deadline: "06/15/2023",
+      flag: Flags.personal),
+  Task(
+      id: 3,
+      body: 'Testing app on multiple devices',
+      status: Status.process,
+      deadline: "07/07/2023",
+      flag: Flags.work),
+  Task(
+      id: 4,
+      body: 'Preparing for presentation',
+      status: Status.process,
+      deadline: "07/15/2023",
+      flag: Flags.work)
 ];
--- todo_app/lib/models/task.dart
+++ todo_app/lib/models/task.dart
@@ -1,9 +1,18 @@
 class Task {
   final int id;
   String body;
+  String deadline;
   Status status;
+  Flags flag;
 
-  Task({this.id = -1, this.body = "No Task", this.status = Status.none});
+  Task(
+      {this.id = -1,
+      this.body = "No Task",
+      this.deadline = "dd/mm/yyyy",
+      this.status = Status.none,
+      this.flag = Flags.none});
 }
 
 enum Status { done, process, archive, none }
+
+enum Flags { personal, work, school, social, none }
