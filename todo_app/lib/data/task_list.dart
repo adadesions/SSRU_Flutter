9042aeb8c3adea125639ffc834f8e01d2c01cb8d
adadesions <adadesions@gmail.com>
2023-05-04 10:23:27 +0700

    Add TaskList

---
+++ todo_app/lib/data/task_list.dart
@@ -0,0 +1,7 @@
+import '../models/task.dart';
+
+List<Task> taskList = [
+  Task(id: 0, body: 'Teaching states in flutter', status: Status.process),
+  Task(id: 1, body: 'Implementing user authentication', status: Status.process),
+  Task(id: 2, body: 'Creating a responsive UI design', status: Status.process),
+];
