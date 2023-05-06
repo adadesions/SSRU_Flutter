028e43e0fc5e958415607bc390dc44064f229f08
adadesions <adadesions@gmail.com>
2023-05-06 11:40:30 +0700

    Task writable to local file

--- db_app/lib/models/task.dart
+++ db_app/lib/models/task.dart
@@ -14,6 +14,10 @@ class Task {
     return Task(task: json['task'], dueDate: json['dueDate']);
   }
 
+  Map<String, dynamic> toJson() {
+    return {'task': task, 'dueDate': dueDate};
+  }
+
   static Future<List<Task>> readFile(String filePath) async {
     final jsonString = await rootBundle.loadString(filePath);
     final jsonList = json.decode(jsonString) as List<dynamic>;
@@ -21,5 +25,13 @@ class Task {
     return jsonList.map((data) => Task.fromJson(data)).toList();
   }
 
-  
+  static void writeFile(List<Task> taskList, String filePath) async {
+    final jsonString =
+        json.encode(taskList.map((task) => task.toJson()).toList());
+    String absolutePath = '/Users/adax/Github/SSRU_Flutter/db_app/$filePath';
+    final file = File(absolutePath);
+    if (await file.exists()) {
+      await file.writeAsString(jsonString);
+    }
+  }
 }
