91e25d346a07adf719034bd024faca618f662a61
adadesions <adadesions@gmail.com>
2023-05-06 10:19:29 +0700

    Add Form to homepage

--- db_app/lib/pages/home_page.dart
+++ db_app/lib/pages/home_page.dart
@@ -8,11 +8,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKeys = GlobalKey<FormState>();
+
+  String _task = '';
+
+  String _dueDate = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Storages'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKeys,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: 'Thing to do...'),
+                    onSaved: (newValue) => {_task = newValue ?? ''},
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'due'),
+                    onSaved: (newValue) => {_dueDate = newValue ?? ''},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
